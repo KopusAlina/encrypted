4601efb9101f1ef0058157c35c84b2cb0efeaaa5
KopusAlina <159190118+KopusAlina@users.noreply.github.com>
2024-06-01 22:14:32 +0300

    1

---
+++ src/Crypt.java
@@ -0,0 +1,36 @@
+import javax.crypto.SecretKey;
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Crypt {
+    public static void main(String[] args) {
+        try {
+            String originalMessage = "Оригинал";
+            System.out.println("Оригинальный текст: " + originalMessage);
+            SecretKey key = AESCipher.generateKey();
+
+            String enCryptMessage = AESCipher.enCrypt(originalMessage, key);
+            System.out.println("Зашифрованный текст: " + enCryptMessage);
+
+            String deCryptMessage = AESCipher.deCrypt(enCryptMessage, key);
+            System.out.println("Расшифрованный текст: " + deCryptMessage);
+
+            Calendar calendar = Calendar.getInstance();
+            SimpleDateFormat originalTime = new SimpleDateFormat("yyyy MMM dd HH:mm:ss");
+            Date now = new Date();
+            String formattedDate = originalTime.format(now);
+            System.out.println("Время отправки: " + formattedDate);
+
+            String enCryptTime = AESCipher.enCrypt(formattedDate, key);
+            System.out.println("Зашифрованное время: " + enCryptTime);
+
+            String deCryptTime = AESCipher.deCrypt(enCryptTime, key);
+            System.out.println("Расшифрованное время: " + deCryptTime);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
